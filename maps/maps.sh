00c74d8680cc0297f2d487f9dff803242f925295
catmint <catmint@kittyware.cc>
2026-09-22 02:04:53 -0500

    Add map sync Dockerfile

---
+++ maps/maps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -xeuo pipefail
+
+cd /data/csgo
+curl -fL http://csgo-kz-maps.badservers.net/downloads/raw-maps.aria2.txt | \
+    aria2c --continue=true --check-integrity=true --auto-file-renaming=false \
+           --max-concurrent-downloads=4 --split=1 --file-allocation=falloc \
+           --conditional-get=true --input-file=-
