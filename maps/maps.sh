43dd7f90d07482ee0ca6516a9719c5b45a0229e3
catmint <catmint@kittyware.cc>
2026-09-23 09:22:19 -0500

    maps: switch to supercronic to fix script output

--- maps/maps.sh
+++ maps/maps.sh
@@ -6,4 +6,5 @@ cd /data/csgo
 curl -fL http://csgo-kz-maps.badservers.net/downloads/raw-maps.aria2.txt | \
     aria2c --continue=true --check-integrity=true --auto-file-renaming=false \
            --max-concurrent-downloads=4 --split=1 --file-allocation=falloc \
+           --show-console-readout=false --enable-color=false \
            --conditional-get=true --input-file=-
